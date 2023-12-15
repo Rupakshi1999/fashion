a7c9af576048a76f4fad86c285811e36844a357f
rupakshi1999 <rupakshiaggarwal2018@gmail.com>
2023-12-15 11:54:49 -0500

    start the server locally

---
+++ app.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+//async errors
+
+//custom error handler middlewares
+const notFound = require('./middleware/not-found');
+const customErrorHandler = require('./middleware/error-handler');
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get('/', (req, res) => {
+  res.send('<h1>Styles API</h1><a href = "/api/v1/styles">API route</a>');
+});
+
+// style routes
+app.use(notFound);
+app.use(customErrorHandler);
+
+// start server
+const start = async () => {
+  try {
+    const port = process.env.PORT || 4000;
+    app.listen(port, () => {
+      console.log(`server is listening on ${port}`);
+    });
+  } catch (err) {}
+};
+
+start();
