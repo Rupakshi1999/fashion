f9973ffa75ad52259790dc4138125f9a781143b1
rupakshi1999 <rupakshiaggarwal2018@gmail.com>
2023-12-16 17:26:58 -0500

    move database query functions into seperate file and add limit to responses

--- controllers/styles.js
+++ controllers/styles.js
@@ -1,47 +1,18 @@
 const Styles = require('../models/styles');
-
-const db__filter = (query) => {
-  const { featured, search, likes, color, createdAt } = query;
-  const queryObject = {};
-
-  if (featured) {
-    queryObject.featured = featured === 'true' ? true : false;
-  }
-  if (search) {
-    queryObject.title = {
-      $regex: search,
-      $options: 'i',
-    };
-  }
-
-  if (color) {
-    queryObject.color = color;
-  }
-
-  return queryObject;
-};
-const db_order = (query) => {
-  const { sort } = query;
-  if (sort) {
-    const sortList = sort.split(',').join(' ');
-    return sortList;
-  } else {
-    return '-createdAt';
-  }
-};
-
-const db_fields = (query) => {
-  const { properties } = query;
-  if (properties) {
-    const porpertyList = properties.split(',').join(' ');
-    return porpertyList;
-  }
-};
+const { db__filter, db_order, db_fields } = require('../db/data');
 
 const getStyles = async (req, res) => {
-  const styles = await Styles.find(db__filter(req.query))
+  let result = Styles.find(db__filter(req.query))
     .sort(db_order(req.query))
     .select(db_fields(req.query));
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  const styles = await result;
   if (!styles) {
     throw new Error(`no data with query ${db__filter}`);
   }
