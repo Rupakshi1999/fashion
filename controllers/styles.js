3975cdc80a5c50ee636bb6c378eb176b85f6c650
rupakshi1999 <rupakshiaggarwal2018@gmail.com>
2023-12-16 16:29:38 -0500

    update name of query

--- controllers/styles.js
+++ controllers/styles.js
@@ -1,6 +1,6 @@
 const Styles = require('../models/styles');
 
-const queryfun = (query) => {
+const db_query = (query) => {
   const { featured, name, likes, color, createdAt } = query;
   const queryObject = {};
 
@@ -19,9 +19,9 @@ const queryfun = (query) => {
 };
 
 const getStyles = async (req, res) => {
-  const styles = await Styles.find(queryfun(req.query));
+  const styles = await Styles.find(db_query(req.query));
   if (!styles) {
-    throw new Error(`no data with query ${queryfun}`);
+    throw new Error(`no data with query ${db_query}`);
   }
   return res.status(200).json({ nmHits: styles.length, data: styles });
 };
