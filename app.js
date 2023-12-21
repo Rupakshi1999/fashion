c1ee4a4b41c5f8a96d49e35a8dd6e414fa6a79e5
rupakshi1999 <rupakshiaggarwal2018@gmail.com>
2023-12-20 19:26:06 -0500

    add security packages to deploy app and setup for hiroku

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
 require('express-async-errors');
 
+// Security packaages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimitter = require('express-rate-limit');
+
 const express = require('express');
 const app = express();
 
@@ -12,9 +18,21 @@ const authenticateUser = require('./middleware/authentication');
 const notFound = require('./middleware/not-found');
 const customErrorHandler = require('./middleware/error-handler');
 
+// Hosting the project on cloud proxy
+app.set('trust proxy', 1);
+// limit each IP to 100 requests per window
+app.use(rateLimitter({ windowMS: 15 * 60 * 1000, max: 100 }));
+
 // middleware
 app.use(express.json());
+// security packages middleware
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
+app.get('/', (req, res) => {
+  res.send('Working with api');
+});
 // auth routes
 const authRouter = require('./routes/auth');
 app.use('/api/v1/auth', authRouter);
