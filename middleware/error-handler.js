3d7fe7c1f4b2cb9ac64f824b839b6b3203b0603c
rupakshi1999 <rupakshiaggarwal2018@gmail.com>
2023-12-20 19:00:07 -0500

    handle mongoose errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,27 @@
-const { CustomAPIError } = require('../errors');
 const { StatusCodes } = require('http-status-codes');
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
+  let Error = {
+    // set default
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    message: err.message || 'Something went wrong, try again later',
+  };
+
+  // mongoose errors
+  if (err.name === 'ValidationError') {
+    Error.statusCode = StatusCodes.BAD_REQUEST;
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+
+  if (err.code && err.code == '11000') {
+    Error.message = `Already taken ${Object.keys(err.keyValue)}`;
+    Error.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  if (err.name === 'CastError') {
+    Error.message = `No item found with ID ${err.value}`;
+    Error.statusCode = StatusCodes.NOT_FOUND;
+  }
+
+  return res.status(Error.statusCode).json({ msg: Error.message });
 };
 
 module.exports = errorHandlerMiddleware;
